eccab2e851048915ab648247b565cdf333ed2d47
asenawritescode <samasena2001@gmail.com>
2023-08-25 21:25:23 +0300

    Staging text

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Hello World, in staging !!');
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${port}`);
 });
